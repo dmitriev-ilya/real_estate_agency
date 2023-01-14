c28f34b73ecbd52c92b373d604d4dc9c230e995f
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-01-14 15:11:47 +0500

    owners added on flats admin page

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Flat, Claim, Owner
 
 
+class FlatOwnersInline(admin.TabularInline):
+    model = Flat.owners.through
+    raw_id_fields = ('owner', 'flat')
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address')
     readonly_fields = ('created_at',)
@@ -16,6 +21,9 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ('liked_by',)
+    inlines = [
+        FlatOwnersInline,
+    ]
 
 
 class ClaimAdmin(admin.ModelAdmin):
